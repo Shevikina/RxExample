bd4d8e6dc35fd32d3f4eb4aee75c0f73d6244f30
Shevikina <shevik.vlog@gmail.com>
2025-07-03 20:35:24 +0300

    Add service for get product by category id

---
+++ app/src/main/java/com/example/rxexample/data/ProductService.kt
@@ -0,0 +1,21 @@
+package com.example.rxexample.data
+
+import com.example.rxexample.data.repositories.CategoryRepositoryImpl
+import com.example.rxexample.data.repositories.ProductRepositoryImpl
+import com.example.rxexample.domain.models.Product
+import com.example.rxexample.domain.repositories.CategoryRepository
+import com.example.rxexample.domain.repositories.ProductRepository
+import io.reactivex.Single
+
+class ProductService(
+    private val categoryRepo: CategoryRepository = CategoryRepositoryImpl(),
+    private val productRepo: ProductRepository = ProductRepositoryImpl()
+) {
+    fun getProductsByCategory(categoryId: Long): Single<List<Product>> {
+        return categoryRepo.getSubcategories(categoryId)
+            .flatMap { subcategories ->
+                val subcategoryIds = subcategories.map { it.id }
+                productRepo.getProductsBySubcategoryIds(subcategoryIds)
+            }
+    }
+}
