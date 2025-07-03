e6225603a46199b3582e1b61c86dd6b879b69c49
Shevikina <shevik.vlog@gmail.com>
2025-07-03 20:36:04 +0300

    Add ui

--- app/src/main/java/com/example/rxexample/MainActivity.kt
+++ app/src/main/java/com/example/rxexample/MainActivity.kt
@@ -4,25 +4,43 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.rxexample.data.ProductService
+import com.example.rxexample.data.data_source.MocSource
+import com.example.rxexample.domain.models.Product
 import com.example.rxexample.ui.theme.RxExampleTheme
+import io.reactivex.schedulers.Schedulers
 
 class MainActivity : ComponentActivity() {
+    private val service by lazy { ProductService() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             RxExampleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    ProductScreen(
+                        modifier = Modifier.padding(innerPadding),
+                        onClickButton = { id, onSuccess, onError ->
+                            service.getProductsByCategory(id)
+                                .observeOn(Schedulers.computation())
+                                .subscribe(
+                                    { onSuccess(it) },
+                                    { error -> onError(error.message) }
+                                )
+                        }
                     )
                 }
             }
@@ -31,17 +49,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ProductScreen(
+    modifier: Modifier = Modifier,
+    onClickButton: (categoryId: Long, onSuccess: (products: List<Product>) -> Unit, onError: (errorMessage: String?) -> Unit) -> Unit
+) {
+    val text = remember { mutableStateOf("Выберите категорию") }
+
+    Column(modifier = modifier) {
+        Text(text = text.value)
+        Row {
+            MocSource.categories.forEach { category ->
+                TextButton(
+                    onClick = {
+                        onClickButton(
+                            category.id,
+                            { products -> text.value = products.map { it.name }.toString() },
+                            { errorMessage -> text.value = "Ошибка: $errorMessage" }
+                        )
+                    }
+                ) { Text(text = category.name) }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ProductScreenPreview() {
     RxExampleTheme {
-        Greeting("Android")
+        ProductScreen { _, _, _ -> }
     }
 }
