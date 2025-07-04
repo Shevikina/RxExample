22487659ceacacc315f7050512f8e3bda8cafa51
Shevikina <shevik.vlog@gmail.com>
2025-07-04 15:28:21 +0300

    Исправлено управление потоками в репозиториях

--- app/src/main/java/com/example/rxexample/data/repositories/CategoryRepositoryImpl.kt
+++ app/src/main/java/com/example/rxexample/data/repositories/CategoryRepositoryImpl.kt
@@ -4,12 +4,11 @@ import com.example.rxexample.data.data_source.MocSource
 import com.example.rxexample.domain.models.Subcategory
 import com.example.rxexample.domain.repositories.CategoryRepository
 import io.reactivex.Single
-import io.reactivex.schedulers.Schedulers
 
 class CategoryRepositoryImpl : CategoryRepository {
     override fun getSubcategories(categoryId: Long): Single<List<Subcategory>> {
-        val subcategories = MocSource.subcategories.filter { it.categoryId == categoryId }
-
-        return Single.just(subcategories).subscribeOn(Schedulers.io())
+        return Single.fromCallable {
+            MocSource.subcategories.filter { it.categoryId == categoryId }
+        }
     }
 }
--- app/src/main/java/com/example/rxexample/data/repositories/ProductRepositoryImpl.kt
+++ app/src/main/java/com/example/rxexample/data/repositories/ProductRepositoryImpl.kt
@@ -4,7 +4,6 @@ import com.example.rxexample.data.data_source.MocSource
 import com.example.rxexample.domain.models.Product
 import com.example.rxexample.domain.repositories.ProductRepository
 import io.reactivex.Single
-import io.reactivex.schedulers.Schedulers
 
 class ProductRepositoryImpl : ProductRepository {
     override fun getProductsBySubcategoryIds(subcategoryIds: List<Long>): Single<List<Product>> {
@@ -12,6 +11,6 @@ class ProductRepositoryImpl : ProductRepository {
             MocSource.products.filter { it.subcategoryId == id }
         }
 
-        return Single.just(products).subscribeOn(Schedulers.io())
+        return Single.just(products)
     }
 }
